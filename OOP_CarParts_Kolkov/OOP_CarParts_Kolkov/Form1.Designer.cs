0122e44bcd90ab73f845d261f6bf67ddcedd96ff
Kolkov Bogdan <bohdan.kolkov@nure.ua>
2026-05-04 18:11:49 +0300

    GUI on main window added. Removed wrong .gitignore file.

--- OOP_CarParts_Kolkov/OOP_CarParts_Kolkov/Form1.Designer.cs
+++ OOP_CarParts_Kolkov/OOP_CarParts_Kolkov/Form1.Designer.cs
@@ -28,12 +28,218 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            splitContainer1 = new SplitContainer();
+            treeView1 = new TreeView();
+            panel1 = new Panel();
+            dataGridView1 = new DataGridView();
+            menuStrip1 = new MenuStrip();
+            файлToolStripMenuItem = new ToolStripMenuItem();
+            довідкаToolStripMenuItem = new ToolStripMenuItem();
+            створитиНовуToolStripMenuItem = new ToolStripMenuItem();
+            завантажитиToolStripMenuItem = new ToolStripMenuItem();
+            зберегтиToolStripMenuItem = new ToolStripMenuItem();
+            зберегтиЯкToolStripMenuItem = new ToolStripMenuItem();
+            закритиToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripSeparator();
+            вихідToolStripMenuItem = new ToolStripMenuItem();
+            навігаціяToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button3
+            // 
+            button3.Location = new Point(12, 606);
+            button3.Name = "button3";
+            button3.Size = new Size(136, 23);
+            button3.TabIndex = 2;
+            button3.Text = " + Додати запчастину";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(154, 606);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 3;
+            button4.Text = "Редагувати";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(235, 606);
+            button5.Name = "button5";
+            button5.Size = new Size(108, 23);
+            button5.TabIndex = 4;
+            button5.Text = " - Видалити";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Location = new Point(12, 41);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(panel1);
+            splitContainer1.Panel2.Controls.Add(dataGridView1);
+            splitContainer1.Size = new Size(1106, 559);
+            splitContainer1.SplitterDistance = 368;
+            splitContainer1.TabIndex = 5;
+            // 
+            // treeView1
+            // 
+            treeView1.Location = new Point(0, 0);
+            treeView1.Name = "treeView1";
+            treeView1.Size = new Size(365, 559);
+            treeView1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(-1, 347);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(735, 212);
+            panel1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(-1, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(735, 341);
+            dataGridView1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem, довідкаToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1130, 24);
+            menuStrip1.TabIndex = 6;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            файлToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { створитиНовуToolStripMenuItem, завантажитиToolStripMenuItem, зберегтиToolStripMenuItem, зберегтиЯкToolStripMenuItem, закритиToolStripMenuItem, toolStripMenuItem1, вихідToolStripMenuItem });
+            файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            файлToolStripMenuItem.Size = new Size(48, 20);
+            файлToolStripMenuItem.Text = "Файл";
+            // 
+            // довідкаToolStripMenuItem
+            // 
+            довідкаToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { навігаціяToolStripMenuItem, aboutToolStripMenuItem });
+            довідкаToolStripMenuItem.Name = "довідкаToolStripMenuItem";
+            довідкаToolStripMenuItem.Size = new Size(61, 20);
+            довідкаToolStripMenuItem.Text = "Довідка";
+            // 
+            // створитиНовуToolStripMenuItem
+            // 
+            створитиНовуToolStripMenuItem.Name = "створитиНовуToolStripMenuItem";
+            створитиНовуToolStripMenuItem.Size = new Size(180, 22);
+            створитиНовуToolStripMenuItem.Text = "Створити нову";
+            // 
+            // завантажитиToolStripMenuItem
+            // 
+            завантажитиToolStripMenuItem.Name = "завантажитиToolStripMenuItem";
+            завантажитиToolStripMenuItem.Size = new Size(180, 22);
+            завантажитиToolStripMenuItem.Text = "Завантажити";
+            // 
+            // зберегтиToolStripMenuItem
+            // 
+            зберегтиToolStripMenuItem.Name = "зберегтиToolStripMenuItem";
+            зберегтиToolStripMenuItem.Size = new Size(180, 22);
+            зберегтиToolStripMenuItem.Text = "Зберегти";
+            // 
+            // зберегтиЯкToolStripMenuItem
+            // 
+            зберегтиЯкToolStripMenuItem.Name = "зберегтиЯкToolStripMenuItem";
+            зберегтиЯкToolStripMenuItem.Size = new Size(180, 22);
+            зберегтиЯкToolStripMenuItem.Text = "Зберегти як";
+            // 
+            // закритиToolStripMenuItem
+            // 
+            закритиToolStripMenuItem.Name = "закритиToolStripMenuItem";
+            закритиToolStripMenuItem.Size = new Size(180, 22);
+            закритиToolStripMenuItem.Text = "Закрити";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(177, 6);
+            // 
+            // вихідToolStripMenuItem
+            // 
+            вихідToolStripMenuItem.Name = "вихідToolStripMenuItem";
+            вихідToolStripMenuItem.Size = new Size(180, 22);
+            вихідToolStripMenuItem.Text = "Вихід";
+            // 
+            // навігаціяToolStripMenuItem
+            // 
+            навігаціяToolStripMenuItem.Name = "навігаціяToolStripMenuItem";
+            навігаціяToolStripMenuItem.Size = new Size(180, 22);
+            навігаціяToolStripMenuItem.Text = "Навігація";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(180, 22);
+            aboutToolStripMenuItem.Text = "About";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1130, 641);
+            Controls.Add(splitContainer1);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private SplitContainer splitContainer1;
+        private TreeView treeView1;
+        private Panel panel1;
+        private DataGridView dataGridView1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem файлToolStripMenuItem;
+        private ToolStripMenuItem створитиНовуToolStripMenuItem;
+        private ToolStripMenuItem довідкаToolStripMenuItem;
+        private ToolStripMenuItem завантажитиToolStripMenuItem;
+        private ToolStripMenuItem зберегтиToolStripMenuItem;
+        private ToolStripMenuItem зберегтиЯкToolStripMenuItem;
+        private ToolStripMenuItem закритиToolStripMenuItem;
+        private ToolStripSeparator toolStripMenuItem1;
+        private ToolStripMenuItem вихідToolStripMenuItem;
+        private ToolStripMenuItem навігаціяToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
     }
 }
